a3d344ee703ee8458c4a4a8d4d7c2572a03bd05c
Paulo Araújo <pauloaraujo.phz@gmail.com>
2023-01-20 02:57:27 -0300

    style(layout): criando Header do projeto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -48,7 +48,46 @@ const Header = () => {
                 </span>
               </div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/compare.svg" alt="Compare" />
+                    <p className="mb-0">
+                      Compare <br />
+                      Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/wishlist.svg" alt="Wishlist" />
+                    <p className="mb-0">
+                      Favorite <br />
+                      wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/user.svg" alt="User" />
+                    <p className="mb-0">
+                      Log in <br />
+                      My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/cart.svg" alt="Cart" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">$ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </header>
